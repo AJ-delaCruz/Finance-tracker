0da1669bb27a6bfb8f4b362a4c3adb854a3f58a3
AJ Dela Cruz <alfredo.delacruz@sjsu.edu>
2023-06-12 12:44:30 -0700

    append "api" to the backendUrl base path

--- client/src/config.js
+++ client/src/config.js
@@ -1,3 +1,3 @@
-export const backendUrl = process.env.REACT_APP_BACKEND_URL || 'http://localhost:3000/api';
+export const backendUrl = process.env.REACT_APP_BACKEND_URL+"api" || 'http://localhost:3000/api';
 export const wsUrl = process.env.REACT_APP_BACKEND_URL || 'http://localhost:3000';
 // export const backendUrl = process.env.REACT_APP_BACKEND_URL;
