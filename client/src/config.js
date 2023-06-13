b550ee699d703219e11e5f9ce3bd1e0b4bc38829
AJ Dela Cruz <alfredo.delacruz@sjsu.edu>
2023-06-12 23:23:05 -0700

    set up backendUrl and wsUrl for frontend

--- client/src/config.js
+++ client/src/config.js
@@ -1,3 +1,2 @@
-export const backendUrl = process.env.REACT_APP_BACKEND_URL || 'http://localhost:3000/api';
-export const wsUrl = process.env.REACT_APP_BACKEND_URL || 'http://localhost:3000';
-// export const backendUrl = process.env.REACT_APP_BACKEND_URL;
+export const backendUrl = process.env.NODE_ENV === 'production' ? process.env.REACT_APP_BACKEND_URL || '/api' : 'http://localhost:3000/api';
+export const wsUrl = process.env.NODE_ENV === 'production' ? process.env.REACT_APP_WS_URL || '/' : 'http://localhost:3000';
