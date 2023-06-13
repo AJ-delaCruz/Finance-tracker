760e94257e0af5c4f15687f73eab013afe95ba45
AJ Dela Cruz <alfredo.delacruz@sjsu.edu>
2023-06-12 23:15:02 -0700

    set url

--- client/src/config.js
+++ client/src/config.js
@@ -1,3 +1,9 @@
-export const backendUrl = process.env.REACT_APP_BACKEND_URL || 'http://localhost:3000/api';
-export const wsUrl = process.env.REACT_APP_BACKEND_URL || 'http://localhost:3000';
-// export const backendUrl = process.env.REACT_APP_BACKEND_URL;
+// export const backendUrl = process.env.REACT_APP_BACKEND_URL || 'http://localhost:3000/api';
+// export const wsUrl = process.env.REACT_APP_BACKEND_URL || 'http://localhost:3000';
+// // export const backendUrl = process.env.REACT_APP_BACKEND_URL;
+
+// export const backendUrl = process.env.NODE_ENV === 'production' ? '/api' : 'http://localhost:3000/api';
+// export const wsUrl = process.env.NODE_ENV === 'production' ? '/' : 'http://localhost:3000';
+
+export const backendUrl = process.env.NODE_ENV === 'production' ? process.env.REACT_APP_BACKEND_URL || '/api' : 'http://localhost:3000/api';
+export const wsUrl = process.env.NODE_ENV === 'production' ? process.env.REACT_APP_WS_URL || '/' : 'http://localhost:3000';
